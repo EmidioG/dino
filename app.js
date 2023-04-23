fef7179fdaa8f4f269b4a9e75fffa67c7ae16ce7
EmidioG <emidiog510@gmail.com>
2023-04-23 16:45:30 -0300

    dinoDown addition

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let dino = document.querySelector("#dino");
 document.addEventListener("keydown", (e) => {
-    if (e.key == " " || e.key == "ArrowUp") {
-        dino.classList.toggle("ativo");
-        console.log(e);
-    } else if (e.key == "ArrowDown") {
-    }
+  if (
+    e.key == " " ||
+    (e.key == "ArrowUp" && dino.classList.contains("dinoDown") === false)
+  ) {
+    dino.classList.toggle("dinoJump");
+  } else if (
+    e.key == "ArrowDown" &&
+    dino.classList.contains("dinoJump") === false
+  ) {
+    dino.classList.toggle("dinoDown");
+  }
 });
